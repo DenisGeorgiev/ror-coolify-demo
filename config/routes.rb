bdc85a3ad79196d48f96e7f9bf2c2f7d9805daa2
Denis Georgiev <d.georgiev@viscomp.bg>
2024-06-08 19:07:53 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
 
-  get "health" => "rails/health#show", as: :rails_health_check
+  get "health" => "rails/health#show", as: :rails_health_check2
 
   # Defines the root path route ("/")
   # root "posts#index"
